1e411d5c08bcde22123133592ec2a4f23ea20255
Nebura <hamklieneduprofesional@gmail.com>
2023-06-06 21:28:45 -0600

    Event store apply, include event modeling

---
+++ CQRS-ES/CQRS.Core/Infrastructure/IEventStore.cs
@@ -0,0 +1,10 @@
+using CQRS.Core.Events;
+
+namespace CQRS.Core.Infrastructure
+{
+    public interface IEventStore
+    {
+        Task SaveEventsAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectVersion);
+        Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId);
+    }
+}
