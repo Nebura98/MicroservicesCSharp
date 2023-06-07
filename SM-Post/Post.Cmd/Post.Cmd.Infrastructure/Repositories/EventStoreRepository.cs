ee557a85f07668c3c78cb0981b5493cab7d83683
Nebura <hamklieneduprofesional@gmail.com>
2023-06-06 21:26:35 -0600

    Event store apply, include event modeling

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Options;
+
+using MongoDB.Driver;
+
+using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using Post.Cmd.Infrastructure.Config;
+
+namespace Post.Cmd.Infrastructure.Repositories
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly IMongoCollection<EventModel> _eventStoreCollectionl;
+
+        public EventStoreRepository(IOptions<MongoDbConfig> config)
+        {
+            var mongoClient = new MongoClient(config.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(config.Value.DataBase);
+
+            _eventStoreCollectionl = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
+        }
+
+        public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+        {
+            return await _eventStoreCollectionl.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task SaveAsync(EventModel @event)
+        {
+            await _eventStoreCollectionl.InsertOneAsync(@event).ConfigureAwait(false);
+        }
+    }
+}
---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Store/EventStore.cs
@@ -0,0 +1,60 @@
+using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregates;
+
+namespace Post.Cmd.Infrastructure.Store
+{
+    public class EventStore : IEventStore
+    {
+        private readonly IEventStoreRepository _eventStoreRepository;
+
+        public EventStore(IEventStoreRepository eventStoreRepository)
+        {
+            _eventStoreRepository = eventStoreRepository;
+        }
+
+        public async Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId)
+        {
+            var eventStream = await _eventStoreRepository.FindByAggregateId(aggregateId);
+
+            if (eventStream == null || !eventStream.Any())
+            {
+                throw new AggregateNotFoundException("Incorrect post ID provided!");
+            }
+
+            return eventStream.OrderBy(x => x.Version).Select(x => x.EventData).ToList();
+        }
+
+        public async Task SaveEventsAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectVersion)
+        {
+            var eventStream = await _eventStoreRepository.FindByAggregateId(aggregateId);
+
+            if (expectVersion != -1 && eventStream[^1].Version != expectVersion)
+            {
+                throw new ConcurrencyException();
+            }
+
+            var version = expectVersion;
+
+            foreach (var @event in events)
+            {
+                version++;
+                @event.Version = version;
+                var eventType = @event.GetType().Name;
+                var eventModel = new EventModel
+                {
+                    TimeStamp = DateTime.Now,
+                    AggregateIdentifier = aggregateId,
+                    AggregateType = nameof(PostAggregate),
+                    Version = version,
+                    EventType = eventType,
+                    EventData = @event
+                };
+
+                await _eventStoreRepository.SaveAsync(eventModel);
+            }
+        }
+    }
+}
