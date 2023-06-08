ce33a5648c6e7ba76ef71ff9b5fe4907a2102d61
Nebura <hamklieneduprofesional@gmail.com>
2023-06-07 19:47:14 -0600

    Event producer, immplementing Apache Kafka

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Producers/EventProducer.cs
@@ -0,0 +1,42 @@
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using Confluent.Kafka;
+
+using CQRS.Core.Events;
+using CQRS.Core.Producers;
+
+namespace Post.Cmd.Infrastructure.Producers
+{
+    public class EventProducer : IEventProducer
+    {
+
+        private readonly ProducerConfig _config;
+
+        public EventProducer(IOptions<ProducerConfig> config)
+        {
+            _config = config.Value;
+        }
+
+        public async Task ProduceAsync<T>(string topic, T @event) where T : BaseEvent
+        {
+            using var producer = new ProducerBuilder<string, string>(_config)
+                .SetKeySerializer(Serializers.Utf8)
+                .SetValueSerializer(Serializers.Utf8)
+                .Build();
+
+            var eventMessage = new Message<string, string>
+            {
+                Key = Guid.NewGuid().ToString(),
+                Value = JsonSerializer.Serialize(@event, @event.GetType())
+            };
+
+            var deliveryResult = await producer.ProduceAsync(topic, eventMessage);
+
+            if (deliveryResult.Status == PersistenceStatus.NotPersisted)
+            {
+                throw new Exception($"Could not produce {@event.GetType().Name} message to topic - {topic} due the following reason: {deliveryResult.Message}");
+            }
+
+        }
+    }
+}
