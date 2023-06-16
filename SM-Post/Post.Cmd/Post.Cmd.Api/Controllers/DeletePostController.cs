dba71216590ac4b2a975746036e7f7ff282f0296
Nebura <hamklieneduprofesional@gmail.com>
2023-06-15 18:38:22 -0600

    Removed unnecessary import

--- SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/DeletePostController.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/DeletePostController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using CQRS.Core.Exceptions;
 using CQRS.Core.Infrastructure;
-using Microsoft.AspNetCore.Mvc;
 using Post.Cmd.Api.Commands;
 using Post.Common.DTOs;
 
